92d624815f1732806d61c1967ad81c436c1b83f3
Kevin Powell <kevin.powell@parachutehealth.com>
2023-05-09 14:51:21 -0500

    Init

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      legacyRootApi: true,
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
